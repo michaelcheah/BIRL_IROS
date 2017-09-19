97e1b9f915dde90f9be4d4797d0b45030f355bdc
michaelcheah <michaelcheah95@gmail.com>
2017-09-19 17:22:54 +0100

    challenge 3 integrated

--- iros_3.py
+++ iros_3.py
@@ -8,6 +8,7 @@ import math
 import cv2
 import imutils
 from matplotlib import pyplot as plt
+import numpy as np
 import os
 
 import iros_interface_cmds as ic
@@ -16,23 +17,24 @@ import iros_waypoints as iw
 
 import iros_vision_tools as ivt
 import iros_vision_functions as ivfunc
+PATH_TO_TASK_IMAGES = "task_images"
 
-def begin(c,ser_ee):
+def begin(c,ser_ee,p1,inverse,CAMERA,crop_points):
     ## Object parameters
-    cup_radius = 25
-    cup_height = 40
-    spoon_bowl = 15         # lenght of spoon bowl (to be convered when stirring)
-    spoon_height = 10
-    stir_radius = cup_radius - 10
-    act_spoon = 10
+    cup_radius = 40
+    cup_height = 60
+    spoon_bowl = 0         # lenght of spoon bowl (to be convered when stirring)
+    spoon_height = 20
+    stir_radius = cup_radius - 20
+    act_spoon = 75
     
     task_img_3 = ivt.capture_pic(CAMERA,1)
     cv2.imwrite(os.path.join(PATH_TO_TASK_IMAGES, 'task_img_3.jpg'), task_img_3)
     
     crop_task_img_3 = ivt.crop_out(task_img_3, crop_points)
     
-    img_3a = task_img_3[0:180, 0:-1]
-    img_3b = ivt.black_out(image, [180,-1,0,-1])
+    img_3a = crop_task_img_3[0:180, 0:-1]
+    img_3b = ivt.black_out(crop_task_img_3, [180,-1,0,-1])
     
     p_circle, spoon = ivfunc.find_spoon(img_3a, show=True)
     
@@ -41,14 +43,19 @@ def begin(c,ser_ee):
     #mx,my,sx,sy = mug_saucer_pos
     
     ## Location of first mug ()
-    p_pix = [p_circle[0],p_circle[1]]
+    p_pix = [p_circle[0][0],p_circle[0][1]]
+    print "P_PIX: ", p_pix
     px,py = ivt.pix3world(p1, inverse, p_pix)
     px = px[0,0]
     py = py[0,0]
     
-    sx,sy = ivt.pix3world(p1, inverse, spoon)
+    print "SPOON: ", spoon
+    s_pix = [spoon[0][0], spoon[0][1]]
+    print "S_PIX: ", s_pix
+    sx,sy = ivt.pix3world(p1, inverse, s_pix)
     sx = sx[0,0]
     sy = sy[0,0]
+    print "PX, PY, SX, SY: ", px, py, sx, sy
     
     p_centre = [px, py]
     p_edge = [sx, sy]
@@ -57,10 +64,12 @@ def begin(c,ser_ee):
     print "P_EDGE:   ", p_edge
     
     ## Location of Second Mug
+    CAL_PARAM = {'thresh': [75, 100],
+                 'radius': [30,45]}
     m_circle, m_cimg = ivt.find_circles(copy.copy(img_3b), 1, param=CAL_PARAM, blur=1, show=False)
-    plt.imshow(cimg)
+    plt.imshow(m_cimg)
     plt.show()
-    m_pix = [m_circle[0],m_circle[1]]
+    m_pix = [m_circle[0][0][0],m_circle[0][0][1]]
     mx,my = ivt.pix3world(p1, inverse, m_pix)
     mx_2 = mx[0,0]
     my_2 = my[0,0]
@@ -73,8 +82,18 @@ def begin(c,ser_ee):
 
     ic.socket_send(c,sCMD=201)
 
+    # Home for end effector and actuator
+    demand_Grip = dict(iw.ee_home)
+    demand_Grip["act"] = act_spoon
+    msg = ic.safe_move(c,ser_ee,Pose=dict(iw.home_joints),Grip=demand_Grip,CMD=2)
+
+    ic.socket_send(c,sCMD=201)
+
     # Goto spoon (TO FINISH)
-    x_p, y_p, ori = get_grasping_coords(p_centre,p_edge)
+    x_p, y_p, ori = get_grasping_coords(p_edge,p_centre)
+    x_p = p_edge[0]
+    y_p = p_edge[1]
+    ori = ori+90
     angle_grasp(c,ser_ee,ori,attack_angle)
 
     current_Joints = ic.get_ur_position(c,3)
@@ -82,10 +101,11 @@ def begin(c,ser_ee):
     msg = ic.safe_ur_move(c,Pose=demand_Joints,CMD=2)
 
     current_Pose = ic.get_ur_position(c,1)
-    demand_Pose = {"x":x_p, "y":y_p, "z":current_Pose[2], "rx":current_Pose[3], "ry":current_Pose[4], "rz":current_Pose[5]} 
+    demand_Pose = {"x":current_Pose[0], "y":current_Pose[1], "z":cup_height+spoon_height, "rx":current_Pose[3], "ry":current_Pose[4], "rz":current_Pose[5]} 
     msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
 
-    demand_Pose["z"]=cup_height+spoon_height
+    demand_Pose["x"]=x_p
+    demand_Pose["y"]=y_p
     msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
 
     # Grasp spoon
@@ -100,6 +120,7 @@ def begin(c,ser_ee):
     demand_Pose = dict(iw.home)
     demand_Pose["x"]=mx_2
     demand_Pose["y"]=my_2
+    demand_Pose["z"]=cup_height+spoon_height+60
     msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
 
     ## Lower spoon
@@ -121,7 +142,7 @@ def begin(c,ser_ee):
     ic.socket_send(c,sCMD=200)
 
     ## Home
-    msg = ic.safe_ur_move(c,Pose=dict(iw.home_joints),CMD=2)
+    msg = ic.safe_move(c,ser_ee,Pose=dict(iw.home_joints),CMD=2)
 
     print ".....................Done......................"
 
@@ -134,7 +155,7 @@ def get_grasping_coords(p_centre,p_edge):
         ori=360+ori
     x = p_edge[0]
     y = p_edge[1]
-    return float(x[0]), float(y[0]), ori
+    return float(x), float(y), ori
 
 
 def angle_grasp(c,ser_ee,orientation,angle_of_attack):
@@ -244,4 +265,3 @@ def angle_grasp(c,ser_ee,orientation,angle_of_attack):
         current_Pose = ic.get_ur_position(c,1)
         demand_Pose = {"x":current_Pose[0],"y":current_Pose[1],"z":current_Pose[2],"rx":rx,"ry":ry,"rz":rz}
         msg = ic.safe_ur_move(c,Pose=dict(demand_Pose),CMD=8)
-
