a1c43517c1d322436342f64974209de33357e672
jaehughes <jaeh2@cam.ac.uk>
2017-09-19 01:11:19 +0100

    Motions for 5 (add angle of attack)

--- iros_5.py
+++ iros_5.py
@@ -14,4 +14,69 @@ import iros_waypoints as iw
 #import vision_copy as vc
 
 def begin(c,ser_ee):
-    print "5"
+
+    # Location of USB top middle
+
+    x_u = 100
+    y_u = 200
+    height_u = 40
+
+    # Location of light
+    x_l = 100
+    y_l = 150
+    height_l = 50
+
+    demand_Grip = dict(ee_home)
+    msg = ic.safe_move(c,ser_ee,Pose=dict(iw.grab_joints),Grip=demand_Grip,CMD=2)
+
+    # Goto position of USB light
+    demand_Pose = dict(home)
+    demand_Pose["x"] = x_u
+    demand_Pose["y"] = y_u
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
+
+    # Lower to USB
+    demand_Pose["z"] = height_u
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
+
+    # Close Gripper
+    demand_Grip["servo"]=10
+    msg = ic.end_effector_move(ser_ee,demand_Grip)
+
+    #Pull out
+    demand_Pose["z"] = height_u + 40
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
+
+    #Push in
+    demand_Pose["z"] = height_u - 5
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
+
+    # Release Gripper
+    demand_Grip["servo"]=80
+    msg = ic.end_effector_move(ser_ee,demand_Grip)
+
+
+    # Goto position of light
+    demand_Pose["x"] = x_l
+    demand_Pose["y"] = y_l
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
+
+    # Lower to light
+    demand_Pose["z"] = height_l
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
+
+    # Close Gripper
+    demand_Grip["servo"]=10
+    msg = ic.end_effector_move(ser_ee,demand_Grip)
+
+    # Pull out
+    demand_Pose["z"] = height_u + 40
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
+
+    # Push in  
+    demand_Pose["z"] = height_u - 5
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
+
+    # Release Gripper
+    demand_Grip["servo"]=80
+    msg = ic.end_effector_move(ser_ee,demand_Grip)
