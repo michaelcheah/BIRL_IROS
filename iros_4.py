bff1b4288e6e73be4573c08ce71ba4a0ff7aeed0
michaelcheah <michaelcheah95@gmail.com>
2017-09-21 16:09:49 +0100

    challenge ready for testing

--- iros_4.py
+++ iros_4.py
@@ -7,93 +7,34 @@ import math
 import cv2
 import imutils
 from matplotlib import pyplot as plt
-import numpy as np
-import os
 
 import iros_interface_cmds as ic
 import iros_waypoints as iw
 #import vision_copy as vc
 
-import iros_vision_tools as ivt
-import iros_vision_functions as ivfunc
-PATH_TO_TASK_IMAGES = "task_images"
+jug_waypoint_joints = {"x": 0.0, "y": 0.0, "z": 0.0, "rx": 0.0, "ry": 0.0, "rz": 0.0}
 
-jug_waypoint_joints_1 = {"x": 84.25, "y": -73.27, "z": 141.24, "rx": -155.85, "ry": -0.83, "rz": -5.47}
-jug_waypoint_joints_2 = {"x": 76.25, "y": -73.27, "z": 141.24, "rx": -155.85, "ry": -0.83, "rz": -5.47}
-
-lift_height = 20
-pour_angle_1 = 60
-unpour = 90
-pour_offset
-
-def begin(c,ser_ee,p1,inverse,CAMERA,crop_points):
+def begin(c,ser_ee):
     act_jug=70
     pour_offset=100
 
     #vision stuff
-    task_img_4 = ivt.capture_pic(CAMERA,1)
-    cv2.imwrite(os.path.join(PATH_TO_TASK_IMAGES, 'task_img_4.jpg'), task_img_4)
-
-    crop_task_img_4 = ivt.crop_out(task_img_4, crop_points)
-    CAL_PARAM = {'thresh': [75, 100],
-                 'radius': [30,45]}
-    m_circle, m_cimg = ivt.find_circles(copy.copy(img_4), 3, param=CAL_PARAM, blur=1, show=False)
-    plt.imshow(m_cimg)
-    plt.show()
-
-    mx=[]
-    my=[]
-    for mug in range(3):
-        m_pix = [m_circle[0][mug][0], m_circle[0][mug][1]]
-        mx_,my_ = ivt.pix3world(p1, inverse, m_pix)
-        mx.append(mx_[0,0])
-        my.append(my_[0,0])
-
-    print "MX: ", mx
-    print "MY: ", my
+    mx=[-600,-600,-600]
+    my=[-200,-300,-400]
 
     #motion stuff: pick mug
-    msg = ic.safe_move(c,ser_ee,Pose=dict(iw.home_joints),Grip=dict(iw.ee_home),CMD=2)
+    # Home
+    demand_Grip = dict(iw.ee_home)
+    demand_Grip["act"]=act_mug
+    msg = ic.safe_move(c,ser_ee,Pose=dict(iw.home_joints),Grip=demand_Grip,CMD=2)    
+    
     # Set tool to iros_1
     ic.socket_send(c,sCMD=201)
 
-    # Go to before jug
-    msg = ic.safe_ur_move(c,Pose=dict(jug_waypoint_joints_1),CMD=2)
+    msg = ic.safe_ur_move(c,Pose=dict(jug_waypoint_joints),CMD=2)
 
-    # Go into jug
-    msg = ic.safe_ur_move(c,Pose=dict(jug_waypoint_joints_2),CMD=2)
-
-    # Glose grabber
-    ic.serial_send(ser_ee,"H",100)
-
-    test = raw_input("wait")
-
-    # Lift up
     current_Pose = ic.get_ur_position(c,1)
-    demand_Pose = dict(current_Pose)
-    demand_Pose["z"] = lift_height
-    msg = ic.safe_ur_move(c,Pose=dict(demand_Pose),CMD=4)
-
-    # Go to location of the cup
-    demand_Pose["x"] = mx[0] + pour_offset
-    demand_Pose["y"] = my[0]
-    msg = ic.safe_ur_move(c,Pose=dict(demand_Pose),CMD=4)
-
-    # Pour
-    current_Joints = ic.get_ur_position(c,3)
-    demand_Joints = dict(current_Joints)
-    demand_Joints["rx"] = pour_angle_1
-    msg = ic.safe_ur_move(c,Pose=dict(demand_Joints),CMD=2)
-
-    time.sleep(1)
-
-    # Stop pour_angle_1
-    demand_Joints["rx"] = unpour
-    msg = ic.safe_ur_move(c,Pose=dict(demand_Pose),CMD=4)
-
-    '''
-    current_Pose = ic.get_ur_position(c,1)
-    demand_Pose = {"x":current_Pose[0], "y":current_Pose[1]+50, "z":current_Pose[2]-50, "rx":current_Pose[3], "ry":current_Pose[4], "rz":current_Pose[5]}
+    demand_Pose = {"x":current_Pose[0], "y":current_Pose[1]+50, "z":current_Pose[2]-50, "rx":current_Pose[3], "ry":current_Pose[4], "rz":current_Pose[5]} 
     demand_Grip = dict(iw.ee_home)
     demand_Grip["servo"]=30
     msg = ic.safe_move(c,ser_ee,Pose=demand_Pose,Grip=demand_Grip,CMD=4)
@@ -112,7 +53,7 @@ def begin(c,ser_ee,p1,inverse,CAMERA,crop_points):
         print "average force: ",full_jug
 
         current_Joints = ic.get_ur_position(c,3)
-        demand_Joints = {"x":current_Joints[0], "y":current_Joints[1], "z":current_Joints[2], "rx":current_Joints[3], "ry":current_Joints[4], "rz":current_Joints[5]}
+        demand_Joints = {"x":current_Joints[0], "y":current_Joints[1], "z":current_Joints[2], "rx":current_Joints[3], "ry":current_Joints[4], "rz":current_Joints[5]} 
 
         j=0
         while fz < full_jug-2 and j<10:
@@ -127,7 +68,7 @@ def begin(c,ser_ee,p1,inverse,CAMERA,crop_points):
         demand_Joints["rz"]=current_Joints[5]
         msg = ic.safe_ur_move(c,Pose=demand_Joints,CMD=2)
 
-    demand_Pose = {"x":current_Pose[0], "y":current_Pose[1]+50, "z":current_Pose[2]+50, "rx":current_Pose[3], "ry":current_Pose[4], "rz":current_Pose[5]}
+    demand_Pose = {"x":current_Pose[0], "y":current_Pose[1]+50, "z":current_Pose[2]+50, "rx":current_Pose[3], "ry":current_Pose[4], "rz":current_Pose[5]} 
     msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
 
     demand_Pose["z"]=current_Pose[2]-50
@@ -137,8 +78,7 @@ def begin(c,ser_ee,p1,inverse,CAMERA,crop_points):
     demand_Pose["y"]=current_Pose[1]
     msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
 
-    msg = ic.safe_ur_move(c,Pose=dict(jug_waypoint_joints),CMD=2
-    '''
+    msg = ic.safe_ur_move(c,Pose=dict(jug_waypoint_joints),CMD=2)
 
     ic.socket_send(c,sCMD=200)
     msg = ic.safe_ur_move(c,Pose=dict(iw.home_joints),CMD=2)
--- iros_5.py
+++ iros_5.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# Scripts for iros challenge 5: unplug main and usb light
+#                               re-plug main and usb light
 import time
 import copy
 import math
@@ -10,96 +13,76 @@ import iros_interface_cmds as ic
 import iros_waypoints as iw
 #import vision_copy as vc
 
-# Pre Determined stuff
-act_usb=70
-pos_u = [-400, -400]            # Location to go to for the USB
-height_u = 80
-
-pos_light = [-450, -400]        #
-height_light = 60               #
-act_light= 20
-
-
 def begin(c,ser_ee):
-    obj = raw_input('Light or USB? (l/u)?')
-    if (obj == "u"):
-        demand_Grip = dict(iw.ee_home)
-        demand_Grip["act"]=act_usb
-        msg = ic.safe_move(c,ser_ee,Pose=dict(iw.home_joints),Grip=demand_Grip,CMD=2)
+    act_usb=70
+    # Location of USB top middle
 
-        # Set tool to iros_1
-        ic.socket_send(c,sCMD=201)
+    x_u = 100
+    y_u = 200
+    height_u = 40
 
-        # Go to waypoint to get correct orientation or the joints to come in from the side to pick
-        demand_Joints = dict(iw.grabbing_joints_waypoint)
-        msg = ic.safe_ur_move(c,Pose=demand_Joints,CMD=2)
+    # Location of light
+    x_l = 100
+    y_l = 150
+    height_l = 50
 
-        current_Pose = ic.get_ur_position(c,1)
-        demand_Pose = {"x":pos_u[0]+5, "y":current_Pose[1], "z":height_u, "rx":current_Pose[3], "ry":current_Pose[4], "rz":current_Pose[5]}
-        msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
+    demand_Grip = dict(iw.ee_home)
+    demand_Grip["act"]=act_usb
+    msg = ic.safe_move(c,ser_ee,Pose=dict(iw.home_joints),Grip=demand_Grip,CMD=2)
 
-        # Move forwards to the USB light
-        demand_Pose["y"] =pos_u[1]
-        msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
+    ## Add Twist
+    current_Pose = get_ur_position(c,CMD=1)
+    new_rx = current_Pose[3] + 20   # Rotate in Z through 180 degrees
+    Pose_rot = {"x":current_Pose[0],"y":current_Pose[1],"z":current_Pose[2],"rx":new_rx,"ry":current_Pose[4],"rz":new_z}
 
-        # Move upto light
-        demand_Pose["x"]=pos_u[0]
-        msg = ic.safe_ur_move(c,Pose=demand_Pose, CMD=4, Speed=0.2)
+    # Goto position of USB light
+    demand_Pose = dict(home)
+    demand_Pose["x"] = x_u
+    demand_Pose["y"] = y_u
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
 
-        # Close Gripper
-        demand_Grip["servo"]=30
-        msg = ic.end_effector_move(ser_ee,demand_Grip)
+    # Lower to USB
+    demand_Pose["z"] = height_u
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
 
-        #Pull out
-        demand_Pose["z"] = height_u + 40
-        msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
+    # Close Gripper
+    demand_Grip["servo"]=30
+    msg = ic.end_effector_move(ser_ee,demand_Grip)
 
-        time.sleep(5)
+    #Pull out
+    demand_Pose["z"] = height_u + 40
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
 
-        #Push in and release
-        demand_Pose["z"] = height_u - 5
-        demand_Grip["servo"]=120
-        msg = ic.safe_move(c,ser_ee,Pose=demand_Pose,Speed=0.25,Grip=demand_Grip,CMD=4)
+    #Push in
+    demand_Pose["z"] = height_u - 5
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
 
-    else:
-        # Home position
-        demand_Grip = dict(iw.ee_home)
-        demand_Grip["act"]=act_light
-        msg = ic.safe_move(c,ser_ee,Pose=dict(iw.home_joints),Grip=demand_Grip,CMD=2)
+    # Release Gripper
+    demand_Grip["servo"]=120
+    msg = ic.end_effector_move(ser_ee,demand_Grip)
 
-        # Set tool to iros_1
-        ic.socket_send(c,sCMD=201)
 
-        # Goto position of light
-        current_Pose = ic.get_ur_position(c,1)
-        demand_Pose = {"x":pos_light[0], "y":pos_light[1], "z":current_Pose[2], "rx":current_Pose[3], "ry":current_Pose[4], "rz":current_Pose[5]}
-        msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
+    # Goto position of light
+    demand_Pose["x"] = x_l
+    demand_Pose["y"] = y_l
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
 
-        # Lower to light
-        demand_Pose["z"] = height_light
-        msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
+    # Lower to light
+    demand_Pose["z"] = height_l
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
 
-        # Close Gripper
-        demand_Grip["servo"]=30
-        msg = ic.end_effector_move(ser_ee,demand_Grip)
+    # Close Gripper
+    demand_Grip["servo"]=30
+    msg = ic.end_effector_move(ser_ee,demand_Grip)
 
-        # Pull out
-        demand_Pose["z"] =  height_light + 50
-        msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
+    # Pull out
+    demand_Pose["z"] = height_u + 40
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
 
-        # Push in and release
-        demand_Pose["z"] =  height_light  - 5
-        demand_Grip["servo"]=120
-        msg = ic.safe_move(c,ser_ee,Pose=demand_Pose,Speed=0.25,Grip=demand_Grip,CMD=4)
+    # Push in
+    demand_Pose["z"] = height_u - 5
+    msg = ic.safe_ur_move(c,Pose=demand_Pose,CMD=4)
 
-    # Raise
-    current_Pose = ic.get_ur_position(c,1)
-    demand_Pose = {"x":current_Pose[0], "y":current_Pose[1], "z":current_Pose[2]+50, "rx":current_Pose[3], "ry":current_Pose[4], "rz":current_Pose[5]}
-
-    # Set tool to iros_0
-    ic.socket_send(c,sCMD=200)
-
-    # Home position
-    msg = ic.safe_move(c,ser_ee,Pose=dict(iw.home_joints),CMD=2)
-
-    print ".....................Done......................"        
+    # Release Gripper
+    demand_Grip["servo"]=120
+    msg = ic.end_effector_move(ser_ee,demand_Grip)
